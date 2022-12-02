9bd577b96ec87422af6e9e7dd9b3ecf0d07953b7
Resteban368 <es.rodriguez@udla.edu.co>
2022-12-02 07:45:16 -0500

    ultimo commit 2/12/22

--- sockets/socket.js
+++ sockets/socket.js
@@ -3,12 +3,30 @@ const { io } = require('../index');
 
 // Mensajes de Sockets
 io.on('connection', client => {
-    console.log('Cliente conectado: ' + client.id);
+    console.log('Cliente conectado token: ', client.handshake.headers['x-token']);
+    //guardamos el id del usuario en el socket
+    client.id = client.handshake.headers['id'];
+    //guardamos el token en el socket
+    client.token = client.handshake.headers['x-token'];
+    //mostar el id del usuario
+    // console.log('id :',client.id);
 
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado: ' + client.id);
+    //ingresa al usuario a una sala en particular
+    //sala global
+    
+    client.join(client.id); //global, client.id, client.handshake.query.sala
+    //escuchar el evento de mensaje personal
+    client.on('conectar-usuario', (payload) => {
+        console.log(payload);
+        
     });
 
+
+
+    client.on('private-message', (payload) => {
+        console.log(payload);
+        io.to(payload.conversacionId).emit('private-message', payload);
+    });
 
 
     client.on('mensaje', (payload) => {
